c52fce8df49286f1e4cdfc6abfc57b94c3d19d89
Dang Khoa <vodangkhoa835@gmail.com>
2024-01-08 01:44:30 +0700

    update

---
+++ SinglePage/SinglePage/src/App.vue
@@ -0,0 +1,56 @@
+<script>
+import HelloVue from './components/Hello.vue';
+export default {
+    name: "App",
+    components:{
+        HelloVue,
+    },
+    data(){
+        return{
+            count: 0
+        }
+    },
+    methods: {
+        onUp(){
+            this.count++;
+        }
+    }
+}
+</script>
+
+
+
+<template>
+    <HelloVue mes="Hello world" 
+        @clickComponent="onUp"
+    />
+    <div>{{ count }}</div>
+</template>
+<style scoped>
+header {
+    line-height: 1.5;
+}
+
+.logo {
+    display: block;
+    margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
+
+    .logo {
+        margin: 0 2rem 0 0;
+    }
+
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
+}
+</style>
---
+++ SinglePage/SinglePage/src/components/Hello.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    name: "Hello",
+    props: {
+        mes: String,
+        
+    },
+    data() {
+        return {
+            name: "OKla",
+            isShow: false
+        }
+    },
+    methods: {
+        onchange(){
+            this.isShow = !this.isShow
+        },
+        onClickComponent(){
+            this.$emit("clickComponent")
+        }
+    },
+    computed: {
+       changeName(){
+        return this.name + "aaaa"
+       }
+    }
+}
+</script>
+
+<template>
+    <div v-if="isShow">
+        {{ changeName }}
+    </div>
+    <button @click="onchange">
+        Click
+    </button>
+    <button @click="onClickComponent">onUp</button>
+    <div>
+        {{ name }}
+    </div>
+    <h5>{{ mes }}</h5>
+</template>
+
+<style scoped></style>
